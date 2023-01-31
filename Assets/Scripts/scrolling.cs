68ab9c5ef99a98610154ad6334499ea1e8ba8338
berke erdem <berkeerdem0@gmail.com>
2023-01-31 21:18:45 +0300

    rte

---
+++ Assets/Scripts/scrolling.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class scrolling : MonoBehaviour
+{
+    [SerializeField] private RawImage _img;
+    [SerializeField] private float x, y;
+
+    void Update()
+    {
+        //rect=konum,yükseklik, genişlik için kullanılır
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(x, y) * Time.deltaTime, _img.uvRect.size);
+    }
+}
