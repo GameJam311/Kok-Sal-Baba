7b14d1392d2a5b527bd9b6719fc032b36d4e11f9
Mustafa <79559036+SirMustafa@users.noreply.github.com>
2023-02-01 00:59:06 +0300

    s

---
+++ Assets/Scripts/Kalp.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Kalp : MonoBehaviour
+{
+    public GameObject kalp1, kalp2, kalp3, yarýmKalp1, yarýmKalp2, yarýmKalp3, bosKalp1, bosKalp2, bosKalp3;
+    private int can = 7;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            can--;
+            if (can == 7)
+            {
+                kalp1.SetActive(true);
+                kalp2.SetActive(true);
+                kalp3.SetActive(true);
+                yarýmKalp1.SetActive(false);
+                yarýmKalp2.SetActive(false);
+                yarýmKalp3.SetActive(false);
+                bosKalp1.SetActive(false);
+                bosKalp2.SetActive(false);
+                bosKalp3.SetActive(false);
+            }
+            if (can == 6)
+            {
+                kalp1.SetActive(true);
+                kalp2.SetActive(true);
+                kalp3.SetActive(false);
+                yarýmKalp1.SetActive(false);
+                yarýmKalp2.SetActive(false);
+                yarýmKalp3.SetActive(true);
+                bosKalp1.SetActive(false);
+                bosKalp2.SetActive(false);
+                bosKalp3.SetActive(false);
+            }
+            if (can == 5)
+            {
+                kalp1.SetActive(true);
+                kalp2.SetActive(true);
+                kalp3.SetActive(false);
+                yarýmKalp1.SetActive(false);
+                yarýmKalp2.SetActive(false);
+                yarýmKalp3.SetActive(false);
+                bosKalp1.SetActive(false);
+                bosKalp2.SetActive(false);
+                bosKalp3.SetActive(true);
+            }
+            if (can == 4)
+            {
+                kalp1.SetActive(true);
+                kalp2.SetActive(false);
+                kalp3.SetActive(false);
+                yarýmKalp1.SetActive(false);
+                yarýmKalp2.SetActive(true);
+                yarýmKalp3.SetActive(false);
+                bosKalp1.SetActive(false);
+                bosKalp2.SetActive(false);
+                bosKalp3.SetActive(true);
+            }
+            if (can == 3)
+            {
+                kalp1.SetActive(true);
+                kalp2.SetActive(false);
+                kalp3.SetActive(false);
+                yarýmKalp1.SetActive(false);
+                yarýmKalp2.SetActive(false);
+                yarýmKalp3.SetActive(false);
+                bosKalp1.SetActive(false);
+                bosKalp2.SetActive(true);
+                bosKalp3.SetActive(true);
+            }
+            if (can == 2)
+            {
+                kalp1.SetActive(false);
+                kalp2.SetActive(false);
+                kalp3.SetActive(false);
+                yarýmKalp1.SetActive(true);
+                yarýmKalp2.SetActive(false);
+                yarýmKalp3.SetActive(false);
+                bosKalp1.SetActive(false);
+                bosKalp2.SetActive(true);
+                bosKalp3.SetActive(true);
+            }
+            if (can == 1)
+            {
+                kalp1.SetActive(false);
+                kalp2.SetActive(false);
+                kalp3.SetActive(false);
+                yarýmKalp1.SetActive(false);
+                yarýmKalp2.SetActive(false);
+                yarýmKalp3.SetActive(false);
+                bosKalp1.SetActive(true);
+                bosKalp2.SetActive(true);
+                bosKalp3.SetActive(true);
+            }
+        }
+    }
+}
